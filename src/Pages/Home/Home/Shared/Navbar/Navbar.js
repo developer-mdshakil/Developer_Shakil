88fd8ad609237bd5b8141f429d1cd21f3e31ca7e
developer-mdshakil <developershakil.cmt1819@gmail.com>
2023-06-09 19:28:36 +0600

    side navbar edit

--- src/Pages/Home/Home/Shared/Navbar/Navbar.js
+++ src/Pages/Home/Home/Shared/Navbar/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import brandImg from '../../../../../assets/brand image/developer_photo-2.png';
-import { FaHome, FaAd, FaServicestack, FaPaperclip, FaBlog, FaFileContract } from 'react-icons/fa';
+import { FaHome, FaAd, FaServicestack, FaPaperclip, FaBlog, FaPhone, FaFacebook, FaLinkedin, FaTwitter, } from 'react-icons/fa';
 
 const Navbar = () => {
     const menuItems = <React.Fragment>
-        <li className='pt-3 pb-3'><Link className='hover:text-red-500 flex'><FaHome className='relative top-1 mr-2 text-lg' />HOME</Link></li>
-        <li className='pt-3 pb-3'><Link className='hover:text-red-500 flex'><FaAd className='relative top-1 mr-2 text-lg' />ABOUT</Link></li>
-        <li className='pt-3 pb-3'><Link className='hover:text-red-500 flex'><FaServicestack className='relative top-1 mr-2 text-lg' />SERVICE</Link></li>
-        <li className='pt-3 pb-3'><Link className='hover:text-red-500 flex'><FaPaperclip className='relative top-1 mr-2 text-lg' />PORTFOLIO</Link></li>
-        <li className='pt-3 pb-3'><Link className='hover:text-red-500 flex'><FaBlog className='relative top-1 mr-2 text-lg' />BLOG</Link></li>
-        <li className='pt-3 pb-3'><Link className='hover:text-red-500 flex'><FaFileContract className='relative top-1 mr-2 text-lg' />CONTACT</Link></li>
+        <li className='pt-3 pb-3'><Link className='hover:text-blue-600 flex'><FaHome className='relative top-1 mr-2 text-lg' />HOME</Link></li>
+        <li className='pt-3 pb-3'><Link className='hover:text-blue-600 flex'><FaAd className='relative top-1 mr-2 text-lg' />ABOUT</Link></li>
+        <li className='pt-3 pb-3'><Link className='hover:text-blue-600 flex'><FaServicestack className='relative top-1 mr-2 text-lg' />SERVICE</Link></li>
+        <li className='pt-3 pb-3'><Link className='hover:text-blue-600 flex'><FaPaperclip className='relative top-1 mr-2 text-lg' />PORTFOLIO</Link></li>
+        <li className='pt-3 pb-3'><Link className='hover:text-blue-600 flex'><FaBlog className='relative top-1 mr-2 text-lg' />BLOG</Link></li>
+        <li className='pt-3 pb-3'><Link className='hover:text-blue-600 flex'><FaPhone className='relative top-1 mr-2 text-lg' />CONTACT</Link></li>
+    </React.Fragment>
+    const socialMenu = <React.Fragment>
+        <li className='w-12 h-12 bg-stone-950 flex justify-center items-center text-2xl shadow shadow-slate-800 rounded-lg'><Link to='https://www.facebook.com/mdshakil.mahamudjoy.3?mibextid=ZbWKwL'><FaFacebook /></Link></li>
+        <li className='w-12 h-12 bg-stone-950 ml-4 flex justify-center items-center text-2xl shadow shadow-slate-800 rounded-lg'><Link><FaLinkedin /></Link></li>
+        <li className='w-12 h-12 bg-stone-950 ml-4 flex justify-center items-center text-2xl shadow shadow-slate-800 rounded-lg'><Link><FaTwitter /></Link></li>
     </React.Fragment>
     return (
         <div className='ml-14'>
@@ -22,6 +27,13 @@ const Navbar = () => {
             <ul className='text-lg text-white'>
                 {menuItems}
             </ul>
+            <div className="social-media text-white mt-28">
+                <hr className='w-44 bg-slate-500' />
+                <h4 className='text-lg font-semibold mt-4 mb-4'>FIND WITH ME</h4>
+                <ul className='flex'>
+                    {socialMenu}
+                </ul>
+            </div>
         </div>
     );
 };
