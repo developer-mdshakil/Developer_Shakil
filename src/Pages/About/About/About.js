60b289dfc793f51c8a1a47956f81897aad27b999
developer-mdshakil <developershakil.cmt1819@gmail.com>
2023-06-13 11:48:00 +0600

    about page setup

--- src/Pages/About/About/About.js
+++ src/Pages/About/About/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import developerPhoto from '../../../assets/brand-image/developer photo.jpg';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About Page</h2>
+        <div className='bg-black w-full h-full ml-1'>
+            <div className='flex justify-between h-full p-6'>
+                <div className='w-full h-full shadow-gray-950 shadow-md text-white'>
+                    <h1 className='text-6xl font-bold mb-4'>hi, I'm <span className='text-blue-600'>Shakil Miah</span></h1>
+                    <p className='text-justify text-lg'>As a skilled Web Developer with expertise in HTML, CSS, JavaScript, React js, Node js, Express js, CSS-3, Tailwind CSS, BootStrap and other web technologies, I have a proven track record of designing and developing high-quality, responsive, and user-friendly websites and web applications. I am experienced in 3+ years front end and have a deep understanding of Frontend Development .I am able to work effectively with clients and team members to deliver solutions that meet their needs.</p>
+                    <div className='mt-12'>
+                        <Link className='bg-black shadow-md shadow-slate-900 w-40 py-4 px-8 text-xl text-red-500 flex rounded-xl mt'>Hire Me<FaArrowRight className='relative top-2 ml-1' /></Link>
+                    </div>
+                </div>
+                <div className='w-full h-full flex justify-center items-center'>
+                    <img src={developerPhoto} alt="Developer Shakil Photo" className='rounded-lg shadow-slate-700 shadow-md' />
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -23,8 +23,8 @@ const Hero = () => {
                         />
                     </span>
                 </h4>
-                <div className='text-center mt-8'>
-                    <button className="btn btn-active btn-neutral hover:-translate-y-1 hover:transition-transform text-red-500 shadow-slate-500 shadow-sm text-lg mr-4">Conact Me</button>
+                <div className='text-center mt-24'>
+                    <button className="btn btn-active btn-neutral hover:-translate-y-1 hover:transition-transform text-red-500 shadow-slate-600 shadow-sm text-lg mr-4">Conact Me</button>
                 </div>
             </div>
         </div>
